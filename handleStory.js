5e08e1ce66dadf5e4d41d0be4df1ecd9a240b0b9
PixelGM <amosprayogo@gmail.com>
2023-08-08 01:01:59 +0000

    Delete the ! message, Message after ! command, User Can't Message Again for The Second time.

--- handleStory.js
+++ handleStory.js
@@ -2,16 +2,19 @@ class CombineStory {
   constructor() {
     this.isCollecting = false;
     this.story = [];
+    this.lastUser = null; // Keep track of the last user
   }
 
   start() {
     this.isCollecting = true;
     this.story = [];
+    this.lastUser = null;
   }
 
-  addWord(word) {
-    if (this.isCollecting) {
+  addWord(word, user) {
+    if (this.isCollecting && (!this.lastUser || this.lastUser !== user)) {
       this.story.push(word);
+      this.lastUser = user;
     }
   }
 
@@ -27,19 +30,30 @@ async function handleStory(message) {
   const content = message.content.trim().toLowerCase();
 
   if (content === "!start") {
+    await message.delete(); // Delete the !start message
     storyCollector.start();
-  } else if (content === "!summarize") {
-    const summary = storyCollector.summarize();
-    message.reply(summary);
+    message.reply("Story collection has started! Add words to the story.");
+  } else if (content === "!sum") {
+    try {
+      await message.delete(); // Delete the !summarize message
+      if (storyCollector.isCollecting) {
+        const summary = storyCollector.summarize();
+        message.reply(summary);
+      } else {
+        message.reply("You need to start the story first! Use the !start command.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   } else if (storyCollector.isCollecting) {
-    if (content.split(" ").length > 1) {
+    if (content.split(" ").length > 1 || storyCollector.lastUser === message.author.id) {
       try {
         await message.delete();
       } catch (error) {
         console.error(error);
       }
     } else {
-      storyCollector.addWord(content);
+      storyCollector.addWord(content, message.author.id);
     }
   }
 }
